fc52eaead7a9dcdd7c101e30f0af68c0dcfa2528
Jonathan Steele <iNoles@users.noreply.github.com>
2024-03-01 14:25:47 -0500

    fixed tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -11,7 +11,12 @@ import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(
+    <SafeAreaProvider>
+      <App />
+    </SafeAreaProvider>,
+  );
 });
